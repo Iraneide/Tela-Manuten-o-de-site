df745248b02ccfba2db3a3219c96f735791190a8
Iraneide <iraneide.ti@gmail.com>
2018-10-17 19:53:54 -0300

    commit

---
+++ embreve/custom.js
@@ -0,0 +1,23 @@
+var preloader = 'on';
+
+var bgImgOverlay = 'on';
+var bgOverlayColour = '#000000';
+var bgOverlayOpacity = '0.4';
+
+var slideshowBackgroundURLS = [
+	"../images/background1.jpg",
+	"../images/background2.jpg",
+	"../images/background2.jpg",
+	"../images/background1.jpg",
+];
+
+var slideshowFade = 1000;
+var slideshowDuration = 4000;
+
+
+var videoBackground = 'on';
+var videoBackgroundURL = 'https://www.youtube.com/watch?v=sBWPCvdv8Bk';
+var videoMuted = true;
+
+
+var countdownDate = "25 AUGUST 2022 00:00:00";
